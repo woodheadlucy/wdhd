a7d8716a79bba4613dd4af732115cd1302a1d070
Lucy Woodhead <woodheadlucy@aol.com>
2021-04-10 18:24:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import About from '../src/pages/about';
+import Community from './pages/community';
+import Contact from './pages/contact';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+
+
+
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Lucy
       </header>
+      <Router>
+        <Link className="menu-item" to="/about">About</Link>
+        <Link className="menu-item" to="/community">In the community</Link>
+        <Link className="menu-item" to="/contact">Contact</Link>
+
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/community">
+        <Community />
+        </Route>
+        <Route path="/contact">
+        <Contact />
+        </Route>
+      </Switch>
+
+      </Router>
+     
+
+
+
+   
     </div>
   );
 }
---
+++ src/pages/community.jsx
@@ -0,0 +1,8 @@
+const Community = () => {
+    return (
+        <h1>Community</h1>
+    )
+}
+
+
+export default Community;
